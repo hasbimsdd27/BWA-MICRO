08120138f81e1ad1ed7b443150613b278e12b841
Hasbi Musaddad <musaddadhasbi@gmail.com>
2021-08-05 14:05:44 +0700

    [hasbi] setup service user

--- service-user/app.js
+++ service-user/app.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const logger = require("morgan");
 const { onError, onListening } = require("./utils/debug");
-const path = require("path");
 
 const Router = require("./routes");
 
 //initial express
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4002;
 const app = express();
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
-app.use("/images", express.static(path.join(__dirname, "./public/images")));
 
 app.use(Router);
 
